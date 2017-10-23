cad67112d5fa7a60647bbac6005b8ac07c93c5f6
ghybs <ghybs1@gmail.com>
2017-10-23 20:28:43 +0400

    First commit
    Functional library.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Manage Libs Versions
+
+Easily create a list of Radio Inputs to let user choose Library Versions,
+retrieve the selection and prepare a list of Assets to be dynamically loaded.
+
+
+## License
+
+Manage Libs Versions library is distributed under the [ISC License](https://choosealicense.com/licenses/isc/).
---
+++ index.js
@@ -0,0 +1,549 @@
+// Adapted from Rollup
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined'
+      ? factory(exports)
+      : typeof define === 'function' && define.amd
+        ? define(['exports'], factory)
+        : (factory((global.manageLibsVersions = {})));
+}(this, (function (exports) {
+  'use strict';
+
+  /**
+   * Create a new Bundle that contains a set of interdependent Libraries.
+   * BundleOptions is an Object with the following expected keys:
+   * - name? <String> mandatory in case BundleOptions is passed as first argument.
+   * - libs? <Array<LibSpec|LibOptions>>
+   * @param bundleName <String|BundleOptions>
+   * @param options? <BundleOptions>
+   * @constructor
+   */
+  function LibsBundle(bundleName, options) {
+    if (typeof bundleName !== 'string') {
+      options = bundleName;
+      bundleName = options.name;
+    }
+    this.name = bundleName;
+    this.libs = [];
+    this.options = options = options || {};
+
+    var self = this,
+        libsData = options.libs || [];
+
+    libsData.forEach(function (libData) {
+      self.addLib(libData);
+    });
+  }
+
+  /**
+   * Add a Library into the current Bundle.
+   * @param libName <string|Library|LibOptions>
+   * @param options? <LibOptions>
+   * @returns {LibSpec}
+   */
+  LibsBundle.prototype.addLib = function (libName, options) {
+    var libSpec = (libName instanceof LibSpec)
+        ? libName
+        : new LibSpec(libName, options);
+
+    libSpec.bundle = this;
+    this.libs.push(libSpec);
+
+    return libSpec;
+  };
+
+  /**
+   * Get the named Library.
+   * @param libName <String>
+   * @returns {LibSpec|undefined}
+   */
+  LibsBundle.prototype.getLib = function (libName) {
+    var libSpecs = this.libs,
+        lib_i = 0;
+
+    for (; lib_i < libSpecs.length; lib_i += 1) {
+      if (libSpecs[lib_i].name === libName) {
+        return libSpecs[lib_i];
+      }
+    }
+
+    console.warn('Could not find requested library "' + libName + '"');
+  };
+
+  /**
+   * Get the named Version of the named Library.
+   * @param libName <String>
+   * @param versionName <String>
+   */
+  LibsBundle.prototype.getLibVersion = function (libName, versionName) {
+    var libSpec = this.getLib(libName);
+
+    if (libSpec) {
+      return libSpec.getVersionFallback(versionName);
+    }
+
+    console.warn('Could not find requested library "' + libName + '" and version "' + versionName + '"');
+  };
+
+  /**
+   * Make a Radio Input for the named Library Version.
+   * @param libName <String>
+   * @param versionName <String>
+   * @param fragment? <Node|DocumentFragment>
+   * @returns {Node|DocumentFragment}
+   */
+  LibsBundle.prototype.makeLibVersionRadio = function (libName, versionName, fragment) {
+    var versionSpec = this.getLibVersion(libName, versionName);
+
+    if (versionSpec) {
+      fragment = versionSpec.makeRadio(fragment);
+    }
+
+    return fragment;
+  };
+
+  /**
+   * Make Radio Inputs for the list of named Library Versions.
+   * @param libName <String>
+   * @param versionsNames <String[]>
+   * @param fragment? <Node|DocumentFragment>
+   * @returns {undefined|Node|DocumentFragment}
+   */
+  LibsBundle.prototype.makeLibVersionsRadios = function (libName, versionsNames, fragment) {
+    var self = this,
+        afterFirst = false;
+
+    versionsNames.forEach(function (versionName) {
+      if (afterFirst) {
+        if (fragment) {
+          fragment.appendChild(document.createElement('br'));
+        }
+      } else {
+        afterFirst = true;
+      }
+      fragment = self.makeLibVersionRadio(libName, versionName, fragment);
+    });
+
+    return fragment;
+  };
+
+  /**
+   * Make and insert Radio Inputs for the list of named Library Versions,
+   * after the script Element where this method is called.
+   * @param libName <String>
+   * @param versionsNames <String[]>
+   * @param fragment? <Node|DocumentFragment>
+   */
+  LibsBundle.prototype.insertLibVersionsRadioHere = function (libName, versionsNames, fragment) {
+    fragment = this.makeLibVersionsRadios(libName, versionsNames, fragment);
+    _insertFragmentHere(fragment);
+  };
+
+  /**
+   * Retrieve the user selected (checked) Radio Inputs, in order to determine
+   * which Library Versions are requested.
+   * @returns {{[Library.name]: Version.name}}
+   */
+  LibsBundle.prototype.readSelectedVersionsNames = function () {
+    var selectedVersionsNames = {};
+
+    this.libs.forEach(function (libSpec) {
+      selectedVersionsNames[libSpec.name] = libSpec.readSelectedVersionName();
+    });
+
+    return selectedVersionsNames;
+  };
+
+  /**
+   * Extract the list of AssetSpecs for the requested Library Versions,
+   * and select (check) the corresponding Radio Inputs.
+   * Used typically to display the requested Libraries Versions and prepare the list
+   * of assets to be loaded (with loadJsCss external library).
+   * @param libVersionsDict <{[Library.name]: Version.name}>
+   * @returns {Array<AssetSpec>}
+   */
+  LibsBundle.prototype.getAndSelectVersionsAssetsList = function (libVersionsDict) {
+    var libSpecs = this.libs,
+        lib_i = 0,
+        assetsLoadList = [],
+        libSpec, versionName, versionSpec;
+
+    for (; lib_i < libSpecs.length; lib_i += 1) {
+      libSpec = libSpecs[lib_i];
+      versionName = libVersionsDict[libSpec.name];
+      versionSpec = libSpec.getAndSelectVersionSpec(versionName);
+      assetsLoadList = assetsLoadList.concat(versionSpec.assets);
+    }
+
+    return assetsLoadList;
+  };
+
+
+  /**
+   * Create a new Library that contains a set of Versions.
+   * LibOptions is an Object with the following expected keys:
+   * - name? <String> mandatory if LibOptions is passed as first argument.
+   * - mandatory? <Boolean> if true, one of the Versions must be set as defaultVersion.
+   * - versions? <Array<VersionSpec|VersionOptions>>
+   * @param libName <String|LibOptions>
+   * @param options? <LibOptions>
+   * @constructor
+   */
+  function LibSpec(libName, options) {
+    if (typeof libName !== 'string') {
+      options = libName;
+      libName = options.name;
+    }
+    this.name = libName;
+    this.options = options = options || {};
+    this.versions = [];
+    this.mandatory = options.mandatory;
+
+    var self = this,
+        versionsData = options.versions || [];
+
+    versionsData.forEach(function (versionData) {
+      self.addVersion(versionData);
+    });
+  }
+
+  /**
+   * Add a Version into the current Library.
+   * @param versionName <String|VersionOptions>
+   * @param options? <VersionOptions>
+   * @returns {VersionSpec}
+   */
+  LibSpec.prototype.addVersion = function (versionName, options) {
+    var versionSpec = (versionName instanceof VersionSpec)
+        ? versionName
+        : new VersionSpec(versionName, options);
+
+    versionSpec.libSpec = this;
+    this.versions.push(versionSpec);
+
+    return versionSpec;
+  };
+
+  /**
+   * Retrieve the user selected (checked) Radio Input, in order to determine
+   * which Version of the current Library is requested.
+   */
+  LibSpec.prototype.readSelectedVersionName = function () {
+    var inputName = _makeLibInputGroupName(this);
+
+    return _getSelectedRadioValue(inputName);
+  };
+
+  /**
+   * Get the named Version, and also select (check) the associated Radio Input.
+   * Used typically to display the requested Version and prepare the list of
+   * assets to be loaded.
+   * @param versionName <String>
+   */
+  LibSpec.prototype.getAndSelectVersionSpec = function (versionName) {
+    var versionSpec = this.getVersionFallback(versionName);
+
+    if (versionSpec) {
+      versionSpec.selectRadio();
+      return versionSpec;
+    }
+
+    this.selectRadioNone();
+  };
+
+  /**
+   * Make a Radio Input that correspond to no Version.
+   * Should not be used when the Library is set as mandatory.
+   * @param fragment? <Node|DocumentFragment>
+   * @returns {Node|DocumentFragment}
+   */
+  LibSpec.prototype.makeRadioNone = function (fragment) {
+    fragment = fragment || document.createDocumentFragment();
+
+    _makeRadioInputAndLabel(this, '(none)', fragment);
+
+    this.radioNoneElement = fragment.lastChild.previousSibling;
+
+    return fragment;
+  };
+
+  /**
+   * Select (check) the Radio Input that corresponds to no Version.
+   */
+  LibSpec.prototype.selectRadioNone = function () {
+    if (this.radioNoneElement) {
+      this.radioNoneElement.checked = true;
+    }
+  };
+
+  /**
+   * Get the named Version, or the default one if the Library is set as
+   * mandatory and no or an incorrect version name is passed.
+   * @param versionName <String>
+   * @returns {Version|undefined}
+   */
+  LibSpec.prototype.getVersionFallback = function (versionName) {
+    // If no version string is provided.
+    if (typeof versionName !== 'string' || versionName === '(none)') {
+      // If library is configured as mandatory, load the default version.
+      if (this.mandatory) {
+        console.warn('No version requested for mandatory library "' + this.name + '".');
+        return this.getDefaultVersion();
+      }
+      return;
+    }
+
+    // Load the requested version.
+    var libVersionSpec = this.getVersion(versionName);
+
+    if (libVersionSpec) {
+      return libVersionSpec;
+    } else {
+      console.warn('Requested library "' + this.name + '" with version "' + versionName + '", but not found in configuration.');
+      return this.getDefaultVersion();
+    }
+  };
+
+  /**
+   * Get the named Version exactly.
+   * If no match is found, returns undefined.
+   * @param versionName <String>
+   * @returns {Version|undefined}
+   */
+  LibSpec.prototype.getVersion = function (versionName) {
+    var versionSpecs = this.versions || [],
+        version_i = 0;
+
+    for (; version_i < versionSpecs.length; version_i += 1) {
+      if (versionSpecs[version_i].name === versionName) {
+        return versionSpecs[version_i];
+      }
+    }
+  };
+
+  /**
+   * Get the default Version.
+   * In case several Versions are set as defaultVersion, the first one is returned.
+   * @returns {Version|undefined}
+   */
+  LibSpec.prototype.getDefaultVersion = function () {
+    var libDefaultVersionSpec = this._getDefaultVersion();
+
+    if (libDefaultVersionSpec) {
+      console.log('Using default configuration (version: "' + libDefaultVersionSpec.name + '").');
+
+      var onDefault = this.options.onDefault;
+
+      if (onDefault && typeof onDefault === 'function') {
+        onDefault.call(this);
+      }
+      return libDefaultVersionSpec;
+    } else {
+      console.warn('No default configuration specified.');
+
+      var onError = this.options.onError;
+
+      if (onError && typeof onError === 'function') {
+        onError.call(this);
+      }
+    }
+  };
+
+  LibSpec.prototype._getDefaultVersion = function () {
+    var versionSpecs = this.versions || [],
+        version_i = 0;
+
+    for (; version_i < versionSpecs.length; version_i += 1) {
+      if (versionSpecs[version_i].defaultVersion) {
+        return versionSpecs[version_i];
+      }
+    }
+  };
+
+
+  /**
+   * Create a Version that contains Assets.
+   * VersionOptions is an Object with the following expected keys:
+   * - name? <String> mandatory in case VersionOptions is passed as first argument.
+   * - defaultVersion? <Boolean> if true, this Version is used as the Library default Version.
+   * - assets? <Array<AssetSpec|AssetOption>>
+   * @param versionName <String|VersionOptions>
+   * @param options? <VersionOptions>
+   * @constructor
+   */
+  function VersionSpec(versionName, options) {
+    if (typeof versionName !== 'string') {
+      options = versionName;
+      versionName = options.name;
+    }
+    this.name = versionName;
+    this.assets = [];
+    this.options = options = options || {};
+    this.defaultVersion = options.defaultVersion;
+
+    var self = this,
+        assetsData = options.assets || [];
+
+    assetsData.forEach(function (assetData) {
+      self.addAsset(assetData);
+    });
+  }
+
+  /**
+   * Add an Asset to the current Version.
+   * @param assetSpec <AssetSpec|AssetOptions>
+   * @returns {Version}
+   */
+  VersionSpec.prototype.addAsset = function (assetSpec) {
+    var assetSpec = (assetSpec instanceof AssetSpec)
+        ? assetSpec
+        : new AssetSpec(assetSpec);
+
+    this.assets.push(assetSpec);
+
+    return this;
+  };
+
+  /**
+   * Make a Radio Input associated with the current Library Version.
+   * @param fragment? <Node|DocumentFragment>
+   * @returns {Node|DocumentFragment}
+   */
+  VersionSpec.prototype.makeRadio = function (fragment) {
+    fragment = fragment || document.createDocumentFragment();
+
+    _makeRadioInputAndLabel(this.libSpec, this.name, fragment);
+
+    this.radioElement = fragment.lastChild.previousSibling;
+
+    return fragment;
+  };
+
+  /**
+   * Select (check) the Radio Input associated with the current Library Version.
+   * Used typically to display the requested Version.
+   */
+  VersionSpec.prototype.selectRadio = function () {
+    if (this.radioElement) {
+      this.radioElement.checked = true;
+    }
+  };
+
+
+  /**
+   * Create an Asset that represents a Resource to be loaded.
+   * AssetOptions is an Object with the following expected keys:
+   * - type <'script'|'stylesheet'> type of Resource (JS or CSS).
+   * - path <String> URL from where to load the Resource.
+   * - attrs <{[attributeName]: attributeValue}> dictionary of
+   *      attributes to apply to the Resource HTML Tag (typically
+   *      for "integrity" and "crossorigin").
+   * @param spec <AssetOptions>
+   * @constructor
+   */
+  function AssetSpec(spec) {
+    this.type = spec.type;
+    this.path = spec.path;
+    this.attrs = spec.attrs || {};
+  }
+
+  function _insertFragmentHere(fragment) {
+    var currentScriptParent = document.currentScript.parentNode;
+    currentScriptParent.appendChild(fragment);
+  }
+
+  function _makeLibInputGroupName(libSpec) {
+    return '_manageLibsVersions_' + libSpec.bundle.name + '_' + libSpec.name;
+  }
+
+  function _getSelectedRadioValue(radioName) {
+    var radios = document.getElementsByName(radioName),
+        i = 0;
+
+    for (; i < radios.length; i += 1) {
+      if (radios[i].checked) {
+        return radios[i].value;
+      }
+    }
+  }
+
+  function _makeRadioInputAndLabel(libSpec, versionName, fragment) {
+    var groupName = _makeLibInputGroupName(libSpec);
+
+    var input = document.createElement('input');
+    input.type = 'radio';
+    input.name = groupName;
+    input.value = versionName;
+    input.id = groupName + versionName;
+    fragment.appendChild(input);
+
+    var label = document.createElement('label');
+    label.setAttribute('for', input.id);
+    label.innerText = versionName;
+    fragment.appendChild(label);
+
+    return fragment;
+  }
+
+
+  /**
+   * Utility function to easily make AssetOptions out of a pathTemplate
+   * and a versionName (typically for CDN URL that includes version, like
+   * for unpkg, cdnjs or jsDelivr).
+   * Type "stylesheet" (CSS).
+   * @param pathTemplate <String> including "{{VESION}}" placeholder.
+   * @param version <String> will replace the "{{VESION}}" placeholder in the above template.
+   * @param sri? <String> Sub Resource Integrity hash
+   */
+  function makeStylesheet(pathTemplate, version, sri) {
+    return _makeAssetSpec('stylesheet', pathTemplate, version, sri);
+  }
+
+  /**
+   * Utility function to easily make AssetOptions out of a pathTemplate
+   * and a versionName (typically for CDN URL that includes version, like
+   * for unpkg, cdnjs or jsDelivr).
+   * Type "script" (JS).
+   * @param pathTemplate <String> including "{{VESION}}" placeholder.
+   * @param version <String> will replace the "{{VESION}}" placeholder in the above template.
+   * @param sri? <String> Sub Resource Integrity hash
+   */
+  function makeScript(pathTemplate, version, sri) {
+    return _makeAssetSpec('script', pathTemplate, version, sri);
+  }
+
+  function _makeAssetSpec(type, pathTemplate, version, sri) {
+    var assetSpec = {
+      type: type,
+      path: _makePath(pathTemplate, version)
+    };
+
+    return _addSri(assetSpec, sri);
+  }
+
+  function _makePath(pathTemplate, version) {
+    return _replaceAll(pathTemplate, '{{VERSION}}', version)
+  }
+
+  function _addSri(assetSpec, integrity, crossorigin) {
+    assetSpec.attrs = assetSpec.attrs || {};
+
+    if (integrity) {
+      assetSpec.attrs.integrity = integrity;
+      assetSpec.attrs.crossorigin = crossorigin || '';
+    }
+
+    return assetSpec;
+  }
+
+  function _replaceAll(target, search, replacement) {
+    return target.split(search).join(replacement);
+  }
+
+  exports.Bundle = LibsBundle;
+  exports.Lib = LibSpec;
+  exports.Version = VersionSpec;
+  exports.Asset = AssetSpec;
+
+  exports.makeScript = makeScript;
+  exports.makeStylesheet = makeStylesheet;
+})));
