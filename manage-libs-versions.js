daed8577692a9259fca7242d0d41a68025019fdf
ghybs <ghybs1@gmail.com>
2018-01-19 20:00:33 +0400

    Style(UMDwrapper): remove indent of chained ternary operator

--- manage-libs-versions.js
+++ manage-libs-versions.js
@@ -8,8 +8,8 @@
   typeof exports === 'object' && typeof module !== 'undefined'
       ? factory(exports)
       : typeof define === 'function' && define.amd
-        ? define(['exports'], factory)
-        : (factory((global.manageLibsVersions = {})));
+      ? define(['exports'], factory)
+      : (factory((global.manageLibsVersions = {})));
 }(this, (function (exports) {
   'use strict';
 
